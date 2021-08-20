a9345cf4c71704690871b9575e008060f76fa0a2
Simona Stroe <sstroe92@gmail.com>
2021-08-20 21:34:54 +0100

    Update SimonGame v1.0

--- javaScript.js
+++ javaScript.js
@@ -2,9 +2,6 @@
 
 
 
-//*** variables
-let currentLevel ;
-let cubes = ["green","red","yellow","blue"];
 
 
 //Capture which button was clicked / animate / play song
@@ -18,7 +15,7 @@ function onButtonClick (){
 
 const changeButtonColor =(element) => {
     
-    console.log(element);
+    // console.log(element);
 
     element.addClass("pressed");
     setTimeout(function(){ (element).removeClass("pressed"); }, 220);
@@ -26,7 +23,7 @@ const changeButtonColor =(element) => {
  
  const makeSoundOnClick =(element)=>{
     
-    console.log(element.attr('id'));
+    //console.log(element.attr('id'));
     var playAudio = new Audio("sounds/"+element.attr('id')+'.mp3');
     playAudio.play();
 
@@ -39,29 +36,86 @@ $(".btn").click(onButtonClick);
 
    $(document).keypress(function(event){
        play();
-    })
-     
-
-function play(){
-
-    if(currentLevel == null){
-    currentLevel = 0;
-    $("#level-title").text("Level " + currentLevel);
+    //    if (levelNr == 0){
+    //        count = 0;
+           
+    //    }
     
-    let randomNumber = Math.floor(Math.random() * 4);
-    let randomCubeId = `#${cubes[randomNumber]}`
+    })
 
-    changeButtonColor($(randomCubeId));
+ //*** variables
+let levelNr = 0;
+let cubes = ["green","red","yellow","blue"];  
+let arrayLevels =[];
+// let arrayUserMemory =[];
+let count = 0;
 
-     
-
-    }
-
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+  function play(){
 
-
+        levelNr += 1;
+        count = 0;
+        
+        $("#level-title").text("Level " + levelNr);
  
+        let randomNumber = Math.floor(Math.random() * 4);
+        let randomCubeId = arrayLevels.push(`${cubes[randomNumber]}`);
+    
+        playCurrentLevel();
+        
 
+
+}
+         $(".btn").click( async function (){  
+                  
+
+                //let addToUserArray = arrayUserMemory.push(this.id);
+                let currentClick = this.id;
+               
+                // console.log("USER "+ currentClick + " PC "+ arrayLevels[count] + " "+ count);   
+                // console.log(currentClick, arrayLevels[count], count, currentClick === arrayLevels[count])
+
+                
+
+                 if(currentClick != arrayLevels[count] ){
+                //    console.log("USER "+ currentClick + " PC "+ arrayLevels[count] + " "+ count); 
+                //    console.log("out");
+                 $("#level-title").text("GameOver, Click To Start Over!")
+                    var playAudio = new Audio("sounds/wrong.mp3");
+                    playAudio.play();
+
+                   return;
+
+                }
+
+
+                if( count == arrayLevels.length -1){
+                    await new Promise(resolve => setTimeout(resolve, 800));
+                    play();
+                    return;
+
+                    };
+
+             
+                count+=1;  
+                
+           });
+
+
+
+async function playCurrentLevel(){
+
+        for (const i of arrayLevels) {
+            //console.log(arrayLevels);
+            await new Promise(resolve => setTimeout(resolve, 800));
+            changeButtonColor($("."+i));
+            makeSoundOnClick($("."+i));
+           
+        }
+}
+       
 
 
